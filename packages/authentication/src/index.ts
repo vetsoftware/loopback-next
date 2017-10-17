bedf553714eaeb0bd70a5a48252970c83e4284c4
Raymond Feng <rfeng@us.ibm.com>
2017-10-17 10:30:31 -0700

    feat(core): add support for extension point/extension
    Extension point/extension is a pattern for extensibility.
    This PR illustrates how we can support it using the Context apis
    and naming conventions. It also serves as an invitation to discuss
    if we should support such entities out of box.

--- packages/authentication/src/index.ts
+++ packages/authentication/src/index.ts
@@ -3,6 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+export * from './authentication';
 export * from './auth-component';
 export * from './decorators/authenticate';
 export * from './keys';
--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -22,7 +22,7 @@ export {
 export {Binding, BindingScope, BindingType} from './binding';
 
 export {Context} from './context';
-export {ResolutionSession} from './resolution-session';
+export {ResolutionSession, ResolutionOptions} from './resolution-session';
 export {
   inject,
   Setter,
